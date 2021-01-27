9e41ef7523e6da644dae751171710d4680fa9cc3
Ankit Maniya <ankit.maniya6588@gmail.com>
2021-01-27 09:28:44 +0530

    add middleware for check all request

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,6 +1,18 @@
 import { config } from "../../config";
 import jwt from "jsonwebtoken";
 
+export const me = async (req, res, next) => {
+  try {
+    if (!req.headers["x-token"]) throw "token is required";
+    const user = await verifyAuthTocken(req.headers["x-token"]);
+    // provide user data to access all route
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(403).send(error);
+  }
+};
+
 export const createToken = async (data, expire) => {
   const { mobile, name } = data;
   const authToken = await jwt.sign({ mobile, name }, config.JWT_SECRET, {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyAuthTocken } from "../functions/auth";
+import { me } from "../functions/auth";
 import { errorRes } from "../functions/helper";
 import User from "./User";
 const route = express.Router();
@@ -7,19 +7,9 @@ const route = express.Router();
 route.get("/", (req, res, next) => {
   res.send("Api called ");
 });
-
 route.use("/user", User);
 
-// route.use(async function (req, res, next) {
-//   try {
-//     if (!req.headers["x-token"]) throw "token is required";
-//     const user = await verifyAuthTocken(req.headers["x-token"]);
-//     // provide user data to access all route
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     res.status(403).send(errorRes(error));
-//   }
-// });
+// verify all requests after called user
+route.use(me);
 
 export default route;
