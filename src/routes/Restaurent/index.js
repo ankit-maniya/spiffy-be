72d4fb1eeb406e08033206445a69f758e7229d84
nitesh <nitesh.mavani@gmail.com>
2021-02-01 19:06:13 +0530

    folder changed

---
+++ src/routes/Restaurent/index.js
@@ -0,0 +1,13 @@
+import Restaurent from "./Restaurent";
+import Menu from "./Menu";
+import Category from "./Category";
+import Item from "./Item";
+
+const Restaurent = {
+    Restaurent,
+    Menu,
+    Category,
+    Item,
+}
+
+export default Restaurent
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,25 @@
 import express from "express";
-import User from "./User";
+import User from "./User/User";
 import Restaurent from "./Restaurent";
-import Menu from "./Menu";
-import Category from "./Category";
-import Item from "./Item";
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
-  res.send("Api called ");
+    res.send("Api called ");
 });
 
 /** User */
 route.use("/user", User);
 
 /** Restaurent */
-route.use("/restaurent", Restaurent);
+route.use("/restaurent", Restaurent.Restaurent);
 
 /** Menu */
-route.use("/menu", Menu);
+route.use("/menu", Restaurent.Menu);
 
 /** Category */
-route.use("/category", Category);
+route.use("/category", Restaurent.Category);
 
 /** Item */
-route.use("/item", Item);
+route.use("/item", Restaurent.Item);
 
 export default route;
